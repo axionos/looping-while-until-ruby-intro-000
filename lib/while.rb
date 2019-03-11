95c2c613ee4b291a1591b87c992c41a32fd29879
SOYEONG OH <axionoso@gmail.com>
2019-03-11 21:16:01 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,13 @@
-def using_while levitation_force = 10
+def using_while
   levitation_force = 6
 
   #your code here
+  if levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force += 10
+  else
 
-  puts "Wingardium Leviosa"
-  levitation_force += 10
+
+
+
 end
